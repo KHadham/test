ac7ea555143be6bc4023ac2cb8a83328352ebc83
khadam ikhwanus shofa <33866110+kampretosjr@users.noreply.github.com>
2019-05-25 16:28:27 +0700

    Update soal4.php

--- soal4.php
+++ soal4.php
@@ -2,16 +2,17 @@
 function cetak_gambar($row){
 	if ($row %2 == "0"){
 	exit('eeee gabisa,harus pake ganjil'); 
+	//program hanya berjalan jika tidak memenuhi syarat if di atas
 	}
 	else
 	for ($i = 1; $i <= $row; $i++){  
-        for ($j = 1; $j <= $row; $j++){  
-            if ( $i==1 ||$i==$row|| $j == ($row/2)+0.5){
-            echo " x ";
-            }
-        else{
-        echo " =";
-        }
+		for ($j = 1; $j <= $row; $j++){  
+		    if ( $i==1 ||$i==$row|| $j == ($row/2)+0.5){
+		    echo " x ";
+		    }
+        	    else{
+             echo " =";
+             }
         }  
         echo "<br>"; 
     }  
