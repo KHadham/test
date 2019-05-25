0ee17fd9fa2b088e3fc3a9874054e18bacc99fc8
khadam ikhwanus shofa <33866110+kampretosjr@users.noreply.github.com>
2019-05-25 15:17:44 +0700

    Create soal4.php

---
+++ soal4.php
@@ -0,0 +1,19 @@
+<?php
+function cetak_gambar($row){
+	if ($row %2 == "0"){
+	exit('eeee gabisa,harus pake ganjil'); 
+	}
+	else
+	for ($i = 1; $i <= $row; $i++){  
+        for ($j = 1; $j <= $row; $j++){  
+            if ( $i==1 ||$i==$row|| $j == ($row/2)+0.5){
+            echo " x ";
+            }
+        else{
+        echo " =";
+        }
+        }  
+        echo "<br>"; 
+    }  
+}
+cetak_gambar(7);
